f18f5f80b043c57d26305a76f78dd7e32d47a05e
JukJerus <94906828+JukJerus@users.noreply.github.com>
2022-11-02 02:13:07 +0700

    Modfied

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,50 +2,302 @@
   <ion-page>
     <ion-header :translucent="true">
       <ion-toolbar>
-        <ion-title>Blank</ion-title>
+        <ion-title style="left: -170px">Home</ion-title>
       </ion-toolbar>
     </ion-header>
-    
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
-    
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-      </div>
+      <!-- Card -->
+      <ion-grid>
+        <ion-row>
+          <ion-col>
+            <ion-card>
+              <ion-card-header>
+                <ion-card-subtitle style="color: black"
+                  >Kolam</ion-card-subtitle
+                >
+                <ion-card-title style="color: black; font-weight: bold"
+                  >10</ion-card-title
+                >
+              </ion-card-header>
+            </ion-card>
+          </ion-col>
+          <ion-col>
+            <ion-card>
+              <ion-card-header>
+                <ion-card-subtitle style="color: black"
+                  >Kolam Aktif</ion-card-subtitle
+                >
+                <ion-card-title style="color: black; font-weight: bold"
+                  >8</ion-card-title
+                >
+              </ion-card-header>
+            </ion-card>
+          </ion-col>
+        </ion-row>
+      </ion-grid>
+
+      <ion-grid>
+        <ion-row>
+          <ion-col>
+            <ion-card>
+              <ion-card-header>
+                <ion-card-subtitle style="color: black"
+                  >Ikan Hidup</ion-card-subtitle
+                >
+                <ion-card-title style="color: black"
+                  ><span style="font-weight: bold">182</span>
+                  Ekor</ion-card-title
+                >
+              </ion-card-header>
+            </ion-card>
+          </ion-col>
+          <ion-col>
+            <ion-card>
+              <ion-card-header>
+                <ion-card-subtitle style="color: black"
+                  >Ikan Mati</ion-card-subtitle
+                >
+                <ion-card-title style="color: black"
+                  ><span>89</span> Ekor</ion-card-title
+                >
+              </ion-card-header>
+            </ion-card>
+          </ion-col>
+        </ion-row>
+      </ion-grid>
+
+      <ion-grid>
+        <ion-row>
+          <ion-col>
+            <ion-card>
+              <ion-card-header>
+                <ion-card-subtitle style="color: black"
+                  >Panen 2022</ion-card-subtitle
+                >
+                <ion-card-title style="color: black"
+                  ><span>109</span> kg</ion-card-title
+                >
+              </ion-card-header>
+            </ion-card>
+          </ion-col>
+          <ion-col>
+            <ion-card>
+              <ion-card-header>
+                <ion-card-subtitle style="color: black"
+                  >Total Pakan</ion-card-subtitle
+                >
+                <ion-card-title style="color: black"
+                  ><span>223</span> kg</ion-card-title
+                >
+              </ion-card-header>
+            </ion-card>
+          </ion-col>
+        </ion-row>
+      </ion-grid>
+
+      <h2 style="margin-left: 10px; margin-top: 10px; margin-bottom: 0">
+        TOTAL BERAT IKAN
+      </h2>
+
+      <!-- slider -->
+      <ion-slides>
+        <ion-slide>
+          <ion-grid>
+            <ion-row>
+              <ion-col style="width: 100%">
+                <ion-card class="kartu">
+                  <img
+                    alt="Silhouette of mountains"
+                    src="https://cdn-icons-png.flaticon.com/512/7591/7591531.png"
+                  />
+                  <ion-card-header>
+                    <ion-card-title style="color: black; font-weight: bold"
+                      >Lele</ion-card-title
+                    >
+                    <ion-card-subtitle style="color: blue"
+                      >200 kg</ion-card-subtitle
+                    >
+                  </ion-card-header>
+                </ion-card></ion-col
+              >
+              <ion-col>
+                <ion-card class="kartu">
+                  <img
+                    alt="Silhouette of mountains"
+                    src="https://cdn-icons-png.flaticon.com/512/2884/2884859.png"
+                  />
+                  <ion-card-header>
+                    <ion-card-title style="color: black; font-weight: bold"
+                      >Nila Merah</ion-card-title
+                    >
+                    <ion-card-subtitle style="color: blue"
+                      >200 kg</ion-card-subtitle
+                    >
+                  </ion-card-header>
+                </ion-card>
+              </ion-col>
+            </ion-row>
+          </ion-grid>
+        </ion-slide>
+        <ion-slide>
+          <ion-card class="kartu">
+            <img
+              alt="Silhouette of mountains"
+              src="https://ionicframework.com/docs/img/demos/card-media.png"
+            />
+            <ion-card-header>
+              <ion-card-subtitle>Card Subtitle</ion-card-subtitle>
+              <ion-card-title>Card Title</ion-card-title>
+            </ion-card-header>
+          </ion-card>
+        </ion-slide>
+      </ion-slides>
+
+      <ion-text color="dark">
+        <h2 style="margin-left: 10px; margin-bottom: 5px; margin-top: 0">KUALITAS AIR</h2>
+      </ion-text>
+      
+      <ion-slides>
+        <ion-slide>
+          <ion-grid>
+            <ion-row>
+              <ion-col>
+                <ion-card>
+                  <ion-card-header>
+                    <ion-card-title
+                      style="
+                        color: black;
+                        right: 50px;
+                        bottom: 10px;
+                        font-weight: bold;
+                      "
+                      >pH</ion-card-title
+                    >
+                    <ion-card-subtitle
+                      style="color: black; right: 12px; bottom: 5px"
+                      >Normal<span style="color: blue">
+                        6 Kolam</span
+                      ></ion-card-subtitle
+                    >
+                    <ion-card-subtitle
+                      style="color: black; right: 3px; bottom: 5px"
+                      >Abnormal<span style="color: blue">
+                        6 Kolam</span
+                      ></ion-card-subtitle
+                    >
+                  </ion-card-header>
+                </ion-card>
+              </ion-col>
+              <ion-col>
+                <ion-card>
+                  <ion-card-header>
+                    <ion-card-title
+                      style="
+                        color: black;
+                        right: 50px;
+                        bottom: 10px;
+                        font-weight: bold;
+                      "
+                      >DO</ion-card-title
+                    >
+                    <ion-card-subtitle
+                      style="color: black; right: 12px; bottom: 5px"
+                      >Normal<span style="color: blue">
+                        6 Kolam</span
+                      ></ion-card-subtitle
+                    >
+                    <ion-card-subtitle
+                      style="color: black; right: 3px; bottom: 5px"
+                      >Abnormal<span style="color: blue">
+                        6 Kolam</span
+                      ></ion-card-subtitle
+                    >
+                  </ion-card-header>
+                </ion-card>
+              </ion-col>
+            </ion-row>
+          </ion-grid>
+        </ion-slide>
+        <ion-slide>
+          <h1>Slide 2</h1>
+        </ion-slide>
+        <ion-slide>
+          <h1>Slide 3</h1>
+        </ion-slide>
+      </ion-slides>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
-import { defineComponent } from 'vue';
+import {
+  IonSlides,
+  IonSlide,
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/vue";
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'HomePage',
+  name: "HomePage",
   components: {
     IonContent,
     IonHeader,
     IonPage,
     IonTitle,
-    IonToolbar
-  }
+    IonToolbar,
+    IonSlides,
+    IonSlide,
+  },
+  setup() {
+    // Optional parameters to pass to the swiper instance. See https://swiperjs.com/swiper-api for valid options.
+    const slideOpts = {
+      initialSlide: 1,
+      speed: 400,
+    };
+    return { slideOpts };
+  },
 });
 </script>
 
 <style scoped>
+img {
+  width: 100%;
+  height: 100%;
+}
+span {
+  font-weight: bold;
+  color: black;
+}
+.kartu {
+  display: flex;
+  flex-direction: column;
+  width: 100% !important;
+  margin: 0 !important;
+  margin-left: 50px;
+  height: 100%;
+}
+
 #container {
   text-align: center;
-  
+
   position: absolute;
   left: 0;
   right: 0;
   top: 50%;
   transform: translateY(-50%);
+}
+
+ion-card {
+  display: flex;
+  flex-direction: column;
+  width: 100% !important;
+  margin: 0 !important;
+  background-color: white;
+  height: 90%;
 }
 
 #container strong {
@@ -54,11 +306,11 @@ export default defineComponent({
 }
 
 #container p {
-  font-size: 16px;
+  font-size: 19px;
   line-height: 22px;
-  
+
   color: #8c8c8c;
-  
+
   margin: 0;
 }
 
---
+++ src/views/Tabs.vue
@@ -0,0 +1,34 @@
+<template>
+    <ion-page>
+      <ion-tabs>
+        <ion-router-outlet></ion-router-outlet>
+        <ion-tab-bar slot="bottom">
+          <ion-tab-button tab="HomePage" href="/tabs/home">
+            <ion-icon :icon="triangle" />
+          </ion-tab-button>
+          <ion-tab-button tab="PageSecond" href="/tabs/Page2">
+            <ion-icon :icon="ellipse" />
+          </ion-tab-button>
+          
+        </ion-tab-bar>
+      </ion-tabs>
+    </ion-page>
+  </template>
+  
+  <script lang="ts">
+  import { defineComponent } from 'vue';
+  import { IonTabBar, IonTabButton, IonTabs, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
+  import { ellipse, triangle } from 'ionicons/icons';
+  
+  export default defineComponent({
+    name: 'TabsPage',
+    components: {IonTabs, IonTabBar, IonTabButton, IonIcon, IonPage, IonRouterOutlet },
+    setup() {
+      return {
+        ellipse, 
+        triangle,
+      }
+    }
+  });
+  </script>
+  
